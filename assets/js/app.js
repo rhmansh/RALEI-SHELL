6f3263819789715a1c26035ec0f48e3822d43489
Rahman Ralei <60416516+rhmansh@users.noreply.github.com>
2023-05-28 16:21:26 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-  var closeButton = $(".close-load");
-  closeButton.on("click", function() {
-    var preloader = $(".preloader");
-    preloader.hide();
-  });
-
   $(window).on("load", function() {
     var preloader = $(".preloader");
-    preloader.delay(1000).fadeOut();
+    preloader.hide();
   });
 });
